10f0ffd6ba958cf31222368099b034508d657057
Pedro Santos <142705026+pedro-santos21@users.noreply.github.com>
2023-09-24 19:36:46 -0300

    Fixed passport.ts auth
    Used perplexity.ai to rewrite the obsolete code that returned "findone doesnt support callback anymore"

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,11 +1,10 @@
-const JwtStrategy = require('passport-jwt').Strategy
+const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const fs = require('fs');
 const path = require('path');
 const User = require('../models/User');
 
-const pathToKey = path.join(__dirname, '..', '..', '..', 'id_rsa_pub.pem');
-const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
+const PUB_KEY = fs.readFileSync(path.resolve(__dirname, process.env.RSA_KEY_PUB_PATH), 'utf8');
 
 // At a minimum, you must pass the `jwtFromRequest` and `secretOrKey` properties
 const options = {
@@ -18,27 +17,22 @@ const options = {
 module.exports = (passport:any) => {
     // The JWT payload is passed into the verify callback
 
-    console.log("* passport is being setup...")
+    console.log("! passport is being setup...")
 
-    passport.use(new JwtStrategy(options, function(jwt_payload:any, done:any) {
+    passport.use(new JwtStrategy(options, async (jwt_payload:any, done:any) => {
 
         console.log(jwt_payload);
         
-        // We will assign the `sub` property on the JWT to the database ID of user
-        User.findOne({_id: jwt_payload.sub}, function(err:any, user:any) {
-            
-            // This flow look familiar?  It is the same as when we implemented
-            // the `passport-local` strategy
-            if (err) {
-                return done(err, false);
-            }
+        try {
+            const user = await User.findOne({_id: jwt_payload.sub});
             if (user) {
                 return done(null, user);
             } else {
                 return done(null, false);
             }
-            
-        });
+        } catch (error) {
+            return done(error, false);
+        }
         
     }));
-}
+};
