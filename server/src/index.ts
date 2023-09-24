a3e2d7de30ecdb2f6c62e7a0adfe2fcc252a040b
Pedro Santos <142705026+pedro-santos21@users.noreply.github.com>
2023-09-23 21:24:52 -0300

    mongoose setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+require('dotenv').config()
 
 const app = express();
 const port = 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
+
+connectToDatabase().catch(err => console.log(err))
+
+async function connectToDatabase() {
+    await mongoose.connect(<string>process.env.MONGODB_KEY);
+    console.log("+ Connected to MongoDB");
+}
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
